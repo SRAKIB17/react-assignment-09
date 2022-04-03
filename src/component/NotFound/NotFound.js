9790a94f45c2e09e75890de77a97c4c3386a7f90
SRAKIB17 <rakibulssc5@gmail.com>
2022-04-04 02:56:36 +0600

    ok 404

--- src/component/NotFound/NotFound.js
+++ src/component/NotFound/NotFound.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import './NotFound.css'
 const NotFound = () => {
     return (
-        <div>
-            
+        <div className='NotFound'>
+            <h1>NOT FOUND</h1>
+            <img src="http://rakib17.hexat.com/icon/busy.gif" alt="" />
         </div>
     );
 };
