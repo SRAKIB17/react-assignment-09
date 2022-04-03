3ea63f6c4e4d137b6bdf1e8bf6ea3054e6bacf87
SRAKIB17 <rakibulssc5@gmail.com>
2022-04-04 03:36:11 +0600

    blogs

--- src/component/Home/CustomerReview/CustomerReview.js
+++ src/component/Home/CustomerReview/CustomerReview.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './CustomerReview.css'
 const CustomerReview = (props) => {
-    const {review:{name, rating, body}} = props
+    const { review: { name, rating, body } } = props
     return (
         <div className='review'>
-            <h3>
-                {name}
-            </h3>
+            <div className='Profile'>
+                <div>
+                    {name.substr(0,1)}
+                </div>
+                <h3>
+                    {name}
+                </h3>
+            </div>
+
             <small>
                 {rating}
             </small>
