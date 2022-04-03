1e6cfeac3600cc29380610d81f42d34f6fdd07f3
SRAKIB17 <rakibulssc5@gmail.com>
2022-04-04 02:42:37 +0600

    updagte chart

--- src/component/Dashboard/ChartBar.js
+++ src/component/Dashboard/ChartBar.js
@@ -3,7 +3,7 @@ import { BarChart, Bar, XAxis, YAxis, Tooltip, CartesianGrid, LineChart } from '
 import useJson from '../../hooks/useReview';
 
 const ChartBar = () => {
-    const data = useJson('chaart')
+    const data = useJson('chart')
     return (<div>
         <BarChart width={600} height={300} data={data}>
             <XAxis dataKey="month" stroke="#8884d8" />
--- src/component/Dashboard/Dashboard.js
+++ src/component/Dashboard/Dashboard.js
@@ -3,10 +3,10 @@ import ChartLine from './LineChart/LineChart';
 
 import ChartPie from './ChartPie';
 import ChartBar from './ChartBar';
-
+import './Dashboard.css'
 const Dashboard = () => {
     return (
-        <div style={{textAlign:'center'}}>
+        <div className='dashboard' style={{textAlign:'center'}}>
             <h2>Month Investment</h2>
             <ChartBar></ChartBar>
             <h2>Month Wish Sell Sell</h2>
